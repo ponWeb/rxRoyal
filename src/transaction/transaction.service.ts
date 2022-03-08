dec67be6470997200a20ac6b80fcc774e3f52076
Tim32 <ponweb@yandex.ru>
2022-03-08 18:31:29 +0300

    move to new node

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -17,8 +17,8 @@ export class TransactionService {
     constructor(@InjectModel(Transaction.name) private transactionModel: Model<TransactionDocument>, @Inject(forwardRef(() => UserService)) private userService: UserService, private configService: ConfigService) {
         this.network = this.configService.get('SOLANA_NETWORK') as Cluster
         this.connection = new Connection(
-            clusterApiUrl(this.network),
-            'confirmed',
+            'https://ssc-dao.genesysgo.net/',
+            'confirmed'
         );
         this.serviceKeypair = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(this.configService.get('KEYPAIR_SECRET_KEY'))))
         console.log('serviceKP:', this.serviceKeypair)
